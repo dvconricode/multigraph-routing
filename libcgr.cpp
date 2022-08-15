dcdb6c81ab75257e03b3c039607b2df30f3b3d21
Conricode <dconrico@ndc.nasa.gov>
2022-08-15 01:49:59 -0400

    frm again

--- libcgr.cpp
+++ libcgr.cpp
@@ -219,7 +219,7 @@ ContactMultigraph::ContactMultigraph(std::vector<Contact> contact_plan, nodeId_t
         }
         else {
             //Vertex frm = vertices[contact.frm];
-            std::vector<Contact> adj = frm.adjacencies[contact.to];
+            std::vector<Contact> adj = vertices[contact.frm].adjacencies[contact.to];
             // if the map can't find the key it creates a default constructed element for it
             // https://stackoverflow.com/questions/10124679/what-happens-if-i-read-a-maps-value-where-the-key-does-not-exist
             if (adj.empty() || contact.start > adj.back().start) {
