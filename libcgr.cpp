718b8b4da6cd52971d6d4ceb6ffa138931288479
Conricode <dconrico@ndc.nasa.gov>
2022-08-15 04:26:26 -0400

    fixed mrp signature

--- libcgr.cpp
+++ libcgr.cpp
@@ -479,7 +479,7 @@ int contact_search_index(std::vector<Contact> contacts, int arrival_time) {
 
  // multigraph review procedure
  // modifies PQ
-void MRP(ContactMultigraph CM, std::priority_queue<Vertex, std::vector<Vertex>, CompareArrivals> PQ, Vertex v_curr) {
+void MRP(ContactMultigraph CM, std::priority_queue<Vertex, std::vector<Vertex>, CompareArrivals> PQ, Vertex* v_curr) {
     for (auto adj : v_curr.adjacencies) {
         Vertex u = CM.vertices[adj.first];
         if (u.visited) {
