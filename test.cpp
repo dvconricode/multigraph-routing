68fb441868fb8a59c9fbbef7b234459b2e19e626
Conricode <dconrico@ndc.nasa.gov>
2022-08-15 02:23:05 -0400

    set up test for route

--- test.cpp
+++ test.cpp
@@ -5,22 +5,36 @@
 using namespace cgr;
 
 int main() {
+
 	std::vector<Contact> contact_plan = cp_load("cgrTutorial.json");
-	
-	std::cout << "---contact plan---" << std::endl;
-	std::cout << contact_plan << std::endl;
-
 	int dest_id = 5;
-	ContactMultigraph cm(contact_plan, dest_id);
+	
+	//std::cout << "---contact plan---" << std::endl;
+	//std::cout << contact_plan << std::endl;
 
-	for (auto& vertex : cm.vertices) {
-		std::cout << "Vertex: " << vertex.first << std::endl;
-		std::cout << "Adjacencies: ";
-		for (auto& adjacency : vertex.second.adjacencies) {
-			std::cout << adjacency.first;
-			std::cout << ", ";
-		}
-		std::cout << std::endl;
+	//ContactMultigraph cm(contact_plan, dest_id);*/
+
+	//for (auto& vertex : cm.vertices) {
+	//	std::cout << "Vertex: " << vertex.first << std::endl;
+	//	std::cout << "Adjacencies: ";
+	//	for (auto& adjacency : vertex.second.adjacencies) {
+	//		std::cout << adjacency.first;
+	//		std::cout << ", ";
+	//	}
+	//	std::cout << std::endl;
+	//}
+
+	Contact rootContact = cgr::Contact(1, 1, 0, cgr::MAX_SIZE, 100, 1.0, 0);
+	rootContact.arrival_time = 0;
+
+	Route bestRoute = cmr_dijkstra(&rootContact, dest_id, contact_plan);
+
+
+	std::vector<Contact> hops = bestRoute.get_hops();
+	int expected_hops = 3;
+
+	for (int i = 0; i < expected_hops; ++i) {
+		std::cout << "Hop " << i << ": " << hops[i] << std::endl;
 	}
 
 	return 0;
