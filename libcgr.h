7fa642228caa6082bf94fcb932d95bd31115a1f4
Conricode <dconrico@ndc.nasa.gov>
2022-08-15 03:56:17 -0400

    changed heap to correctly be min heap

--- libcgr.h
+++ libcgr.h
@@ -92,7 +92,7 @@ class CompareArrivals
 public:
     bool operator()(const Vertex& v1, const Vertex& v2)
     {
-        return v1.arrival_time < v2.arrival_time;
+        return v1.arrival_time > v2.arrival_time;
     }
 };
     int contact_search_index(std::vector<Contact> contacts, int arrival_time);
